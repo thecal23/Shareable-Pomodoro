de324e0b47e286ac2d5c18430bdebba61a8b5cc6
thecal23 <thecal23@gmail.com>
2022-03-02 23:13:20 -0500

    Added a reset button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let start = document.getElementById('action-btn')
+let reset = document.getElementById('reset-btn')
 
 let timeString = document.getElementById('time-string')
 
@@ -8,9 +9,11 @@ let sec = 00;
 let startTimer;
 
 start.addEventListener('click', Start)
+reset.addEventListener('click', Reset)
 
+//Start Button
 function Start(){
-    start.removeEventListener('click', Start);
+    start.removeEventListener('click', Start);   //Replace Start button with Stop button
     start.addEventListener('click',Stop);
     start.innerText =`Stop`
     if(startTimer === undefined){
@@ -20,12 +23,23 @@ function Start(){
     }
 }
 
+//Stop Button
 function Stop(){
-    start.removeEventListener('click',Stop);
+    start.removeEventListener('click',Stop);    //Replace Stop button with Start Button
     start.addEventListener('click',Start);
     start.innerText=`Start`;
     stopInterval();
     startTimer = undefined;
+}
+
+//Reset button
+function Reset(){
+    min = 25;
+    sec = 00;
+
+    stopInterval();
+    startTimer = undefined;
+    timeString.innerText = `${min}:0${sec}`;
 }
 
 function timer(){
@@ -34,7 +48,7 @@ function timer(){
     if(sec != 0){
         sec--;
         if (min!=0 && sec <10){
-            timeString.innerText = `${min}:0${sec}`
+            timeString.innerText = `${min}:0${sec}`;
         } else{
             timeString.innerText = `${min}:${sec}`;
         }
@@ -65,4 +79,11 @@ function timer(){
 
 function stopInterval(){
     clearInterval(startTimer);
+}
+
+inputEnter = document.getElementById("#basic-addon1")
+inputEnter.addEventListener('click', checkForClick)
+
+function checkForClick(){
+    inputEnter.classList.toggle("hidden");
 }
